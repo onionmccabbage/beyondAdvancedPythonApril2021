a5f734d4db82d5857a2f5cfb7eedd4e98ff294dd
onionmccabbage <onionmccabbage@live.com>
2021-04-28 13:50:33 +0100

    review2

--- review2/review2_threads.py
+++ review2/review2_threads.py
@@ -3,31 +3,16 @@ import json
 import requests
 import time
 
+from weatherService import TempGetter
+
 # CITIES = [
 #     'Athlone', 'Dublin', 'Galway', 'Belfast', 
 #     'London', 'Cork', 'Lund', 'Kista'
 # ]
 CITIES = [
-    'Athlone', 'Canberra', 'london', 'athens'
+    'Athlone', 'Canberra', 'London', 'Athens'
 ]
 # CITIES = ["Dublin","Cork","Limerick","Galway","Waterford","Drogheda","Kilkenny","Wexford","Sligo","Clonmel","Dundalk","Bray","Ennis","Tralee","Carlow","Naas","Athlone","Letterkenny","Tullamore","Killarney","Arklow","Cobh","Castlebar","Midleton","Mallow","Ballina","Enniscorthy","Wicklow","Cavan","Athy","Longford","Dungarvan","Nenagh","Trim","Thurles","Youghal","Monaghan","Buncrana","Ballinasloe","Fermoy","Westport","Carrick-on-Suir","Birr","Tipperary","Carrickmacross","Kinsale","Listowel","Clonakilty","Cashel","Macroom","Castleblayney","Kilrush","Skibbereen","Bundoran","Templemore","Clones","Newbridge","Mullingar","Balbriggan","Greystones","Leixlip","Tramore","Shannon","Gorey","Tuam","Edenderry","Bandon","Loughrea","Ardee","Mountmellick","Bantry","Boyle","Ballyshannon","Cootehill","Ballybay","Belturbet","Lismore","Kilkee","Granard"]
-
-class TempGetter(Thread):
-    def __init__(self, city):
-        super().__init__()
-        self.city = city
-
-    def run(self):
-        url_template = (
-            'http://api.openweathermap.org/data/2.5/'
-            'weather?q={}&units=metric&APPID=48f2d5e18b0d2bc50519b58cce6409f1')
-        response = requests.get(url_template.format(self.city))
-        data = json.loads(response.text)
-        self.temperature = data['main']['temp']
-        lon = 0
-        lat = 52
-        r = requests.get('https://www.google.co.uk/maps/place/{},{}'.format(lon, lat))
-        print(r.text)
 
 threads = [TempGetter(c) for c in CITIES]
 start = time.time()
